915465779deaacf6e56c61e90f8fbdc821e527f5
piyushpatil2002 <68774547+piyushpatil2002@users.noreply.github.com>
2023-06-18 16:15:49 +0530

    Time: 314 ms (53.83%), Space: 116.1 MB (57.8%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int>st;
+        for(auto it: candyType){
+            st.insert(it);
+        }
+        return min(st.size(), candyType.size()/2);
+    }
+};
